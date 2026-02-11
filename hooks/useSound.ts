cd2e0b7b4fdcd8dcd0944090582f0bee59abedcf
Dandy <dandyraspudi@gmail.com>
2026-02-11 14:01:55 +0700

    del unused code

--- hooks/useSound.ts
+++ hooks/useSound.ts
@@ -1,29 +1,3 @@
-// "use client";
-
-// import { useRef } from "react";
-
-// export function useSound(src: string, volume = 0.5) {
-//   const audioRef = useRef<HTMLAudioElement | null>(null);
-
-//   if (!audioRef.current) {
-//     audioRef.current = new Audio(src);
-//     audioRef.current.volume = volume;
-//   }
-
-//   // const play = () => {
-//   //   audioRef.current!.currentTime = 0;
-//   //   audioRef.current!.play();
-//   // };
-//   const play = () => {
-//     if (typeof window === "undefined") return;
-//     const audio = new Audio(src);
-//     audio.volume = volume;
-//     audio.play().catch(() => {});
-//   };
-
-//   return play;
-// }
-
 import { useRef, useEffect } from "react";
 
 export function useSound(src: string, volume: number = 1) {
